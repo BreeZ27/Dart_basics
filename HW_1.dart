c8f99db506bf5ae64d2c2b27aa5b3f8ee3217410
BreeZ27 <khavlaan@gmail.com>
2021-09-18 14:14:28 +0300

    Update HW_1.dart

--- HW_1.dart
+++ HW_1.dart
@@ -13,28 +13,57 @@ int nod (int a, int b){
   return c;
 }
 
-// Конвертер чисел из десятичного вида в двоичный и обратно. Принимает число для конвертации и bool, определяющий направление конвертации (true - из Decimal ещ binary). 
-binConv(int a, bool back){
-  List<int> bin = [];
+// Разбивает число на простые множители и возвращает их.
+to_simple(int a){
+ 
+  List<int> res = [1];
   var b = a;
-  var res;
+  var i;
+  
+  do {
+    for (i = 2; i <= a; i++){
+      if (b%i == 0){
+        print('В результате деления $b на $i получили:');
+        b = b~/i;
+        print(b);
+        res.add(i);
+        break;
+      }
+    }
+  } while (i < b);
+  return res;
+}
+
+// Конвертер чисел из десятичного вида в двоичный и обратно. 
+// Принимает число для конвертации и bool, определяющий направление конвертации (true - из Decimal в binary). 
+binConv(int a, bool back){
+  var b = a;
+  num res = 0;
   var subRes;
   var i;
   var k;
   
-  if (back = false){
-    while (b~/2 != 0){
-    bin.add(b%2);
-    b = b~/2;}
+  if (back == false) {
+    List<int> bin = [];
+    while (b~/2 != 0) {
+      bin.add(b%2);
+      b = b~/2;
+    }
     if (b%2 == 1){bin.add(1);}
-    res = int.parse(bin.reversed.toString().replaceAll('(','').replaceAll(', ','').replaceAll(')',''));
+    res += int.parse(bin.reversed.toString().replaceAll('(','').replaceAll(', ','').replaceAll(')',''));
   }
+
   else{
-    for (i=b.toString().length-1; i==0; i--){
-      subRes = 1;
-      for (k=i; k==1; k--){subRes *= 2;}
-      res += subRes;
-      print(res);
+    List bin = [];
+    bin = b.toString().split('').toList().reversed.toList();
+    for (i=0; i<bin.length; i++){
+      if (bin[i] == '1'){
+        subRes = 1;
+        for (k=i; k > 0; k--){
+          subRes *= 2;
+        }
+        res += subRes;
+      }
     }
   }
   return res;
@@ -43,6 +72,8 @@ binConv(int a, bool back){
 
 
 void main() {
-  print(binConv(12, false));
-  print(binConv(12, true));
+  // print(to_simple(126));
+  // print(binConv(12, false));
+  // print(binConv(110, true));
+
 }
